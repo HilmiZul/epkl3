7af289533561df1c427934104c00e1c2fea6c7cf
hilmizul <netspytux@gmail.com>
2019-09-23 11:31:44 +0700

    list display

--- karya_ilmiah/admin.py
+++ karya_ilmiah/admin.py
@@ -3,14 +3,14 @@ from .models import SubmissionJudul, Bimbingan
 
 # Register your models here.
 class SubmissionJudulAdmin(admin.ModelAdmin):
-  list_display = ['judul', 'tanggal', 'tanggal_acc', 'submit_status', 'status', 'siswa', 'pembimbing']
+  list_display = ['judul', 'tanggal_acc', 'submit_status', 'status', 'siswa', 'verified_by']
   list_filter = ('submit_status', 'status',)
   search_fields = ['judul', 'siswa', 'pembimbing']
   list_per_page = 20
 
 
 class BimbinganAdmin(admin.ModelAdmin):
-  list_display = ['judul', 'bab', 'tanggal', 'status']
+  list_display = ['judul', 'bab', 'tanggal', 'status', 'verified_by']
   list_filter = ('status',)
   search_fields = ['judul']
   list_per_page = 20
