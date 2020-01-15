8c792414834e5993a084018f68fa19f72b14586b
Zul Hilmi <netspytux@gmail.com>
2020-01-15 17:46:17 +0700

    change db

--- epkl3/settings.py
+++ epkl3/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'epkl3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'epkl',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
