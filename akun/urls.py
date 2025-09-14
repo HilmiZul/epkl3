e929f2b4568cd7782a5b5c8dd8e1aac20f973a2c
hilmizul <netspytux@gmail.com>
2025-09-14 22:47:11 +0700

    update model and url akun

--- akun/urls.py
+++ akun/urls.py
@@ -5,11 +5,11 @@ from dashboard.views import dashboard_siswa
 
 urlpatterns = [
   path('home/', dashboard_siswa),
-  
+
   path('', masuk),
   path('masuk/', masuk),
   path('keluar/', keluar),
-  
+
   # akun siswa
   path('login/', masuk_siswa),
   path('logout/', keluar_siswa),
